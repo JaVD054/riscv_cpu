613c4fb6607cc4aafa5f0991e473a98cf52f096b
JaVD054 <jeedu124@gmail.com>
2023-11-04 15:21:23 +0530

    lw fixed

--- verilog_codes/sgn_zero_extend.v
+++ verilog_codes/sgn_zero_extend.v
@@ -5,16 +5,18 @@ module sgn_zero_extend(
 );
 
 always @(*) begin
-    case({funct3[2], funct3[0]})
+    case(funct3)
         // lb
-        2'b00: ext_out = {{24{read_data_mem[7]}}, read_data_mem[7:0]};
+        2'b000: ext_out = {{24{read_data_mem[7]}}, read_data_mem[7:0]};
         // lh
-        2'b01: ext_out = {{16{read_data_mem[15]}}, read_data_mem[15:0]};
+        2'b001: ext_out = {{16{read_data_mem[15]}}, read_data_mem[15:0]};
         // lbu
-        2'b10: ext_out = {24'b0, read_data_mem[7:0]};
+        2'b100: ext_out = {24'b0, read_data_mem[7:0]};
         // lhu
-        2'b11: ext_out = {16'b0, read_data_mem[15:0]};
-        default: ext_out = 32'bxxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx; // undefined
+        2'b101: ext_out = {16'b0, read_data_mem[15:0]};
+        // lw
+        2'b010: ext_out = read_data_mem;
+        default: ext_out = read_data_mem; // undefined
     endcase
 end
 
