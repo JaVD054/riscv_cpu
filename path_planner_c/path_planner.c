4713762b7e32a453c804b7a90eaf5dfdad1a1c4b
JaVD054 <jeedu124@gmail.com>
2023-11-06 12:45:30 +0530

    updated working path_planner.c

--- path_planner_c/path_planner.c
+++ path_planner_c/path_planner.c
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define V 30
+#define INF 0xff
+
 #ifdef __linux__ // for host pc
 
     #include <stdio.h>
@@ -59,16 +62,37 @@ void bit_load(uint32_t *n, int i){
     *n |= 0x20000000 >> i;
 }
 
-int array_index(uint32_t *arr, uint8_t n) {
-    return (arr[(n & 0xfc)>>2]&(0XFF000000>>((n&0x3)*8)))>>(~(n&0x3)*8);
+uint8_t array_index(uint32_t *arr, uint8_t n) {
+    return (arr[(n & 0xfc)>>2]&(0XFF000000>>((n&0x3)*8)))>>(~(n&0x3)*8); 
 } 
 
 void array_write(uint32_t *arr, uint8_t n, uint8_t val) {
     arr[(n & 0xfc)>>2] &= ~(0XFF000000>>((n&0x3)*8));
     arr[(n & 0xfc)>>2] |= val << (~(n&0x3)*8);
-    printf("arr: %x\n", arr[(n & 0xfc)>>2] );
+    // printf("arr: %x\n", arr[(n & 0xfc)>>2] );
 }
 
+uint8_t minDistance(uint32_t dist[], uint32_t sptSet)
+{
+	int min = INF, min_index;
+
+	for (int v = 0; v < V; v++)
+		if (bit_position(sptSet,v) == false && array_index (dist,v) <= min)
+			min = array_index (dist,v), min_index = v;
+
+	return min_index;
+}
+
+void findPath(uint8_t currentVertex, uint32_t parents[], uint8_t path_planned[], uint8_t *idx)
+{
+
+    if (currentVertex == 0xff) {
+        return;
+    }
+    findPath(array_index(parents,currentVertex), parents, path_planned, idx);
+    
+    path_planned[(*idx)++] = currentVertex;
+}
 
 
 // main function
@@ -130,25 +154,54 @@ int main(int argc, char const *argv[]) {
     map[28] = 0b000100000000000000000000001001;
     map[29] = 0b010000000000000000001000000010;
 
-    uint32_t visited = 0b0;
+    uint32_t visited = 0x00000000;
     uint32_t dist[8], prev[8];
 
     for (int i = 0; i < 30; ++i) {
-        array_write(dist, i, 0xff);
+        array_write(dist, i, INF);
     }
 
     array_write(dist, START_POINT, 0);
     array_write(prev, START_POINT, 0xff);
-    NODE_POINT = bit_position(map[START_POINT], 5);
+
+
+
+    for (int i = 0; i < V - 1; i++) {
+		uint8_t u = minDistance(dist, visited);
+
+        //break if the destination is reached
+        if (u == END_POINT)
+            break;
+        //mark the vertex as visited
+		bit_load(&visited,u);
+
+        // updating the distance of the adjacent unvisited vertices
+		for (int v = 0; v < V; v++)
+			if (!bit_position (visited,v) && bit_position (map[u],v)   // if the vertex is not visited (value of current v has never been the value of u) 
+                                            //and there is an edge between u and v
+				&& array_index(dist,u) != INF  // if the distance of u is not infinity
+				&& array_index(dist,u) + bit_position (map[u],v) < array_index(dist,v)) // sum of distance to u and edge weight of v-u is 
+                                                    //less than the distance to v (v is not visited)
+                {
+                    array_write(dist,v,array_index(dist,u) + bit_position (map[u],v)); //update the distance of v     
+                    // printf("dist: %d\n", array_index(dist,v));
+                    array_write(prev,v, u); //update the parent of v
+                }
+	}
+
+    findPath(END_POINT, prev, path_planned, &idx);
+
+
 
 
 
     // ##############################################
 
     // the node values are written into data memory sequentially.
-    // for (int i = 0; i < idx; ++i) {
-    //     NODE_POINT = path_planned[i];
-    // }
+    for (int i = 0; i < 1; ++i) {
+        NODE_POINT = path_planned[i];
+    }
+
     // Path Planning Computation Done Flag
     CPU_DONE = 1;
 
